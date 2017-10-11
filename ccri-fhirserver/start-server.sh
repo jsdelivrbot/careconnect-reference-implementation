5f9ea8a235bcc49a81479676654b4d4a808a5c72
Andy Bowes <48afe687@opayq.com>
2017-10-11 12:33:48 +0100

    Interim Checkin - Tomcat Startup Fails

--- ccri-fhirserver/start-server.sh
+++ ccri-fhirserver/start-server.sh
@@ -1,14 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "Waiting for mysql"
-until mysql -hccrisql -P3306 -u"${datasource.username}" -p"${datasource.password}" -e "select 1;"&> /dev/null
-do
-  printf "."
-  sleep 3
-done
+#echo "Waiting for mysql"
+#MYSQL_USER=$1
+#MYSQL_PASSWORD=$2
+#echo "Database Username: $MYSQL_USER"
+#echo "Database Password: $MYSQL_PASSWORD"
+#until mysql -hccrisql -P3306 -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "select 1;"&> /dev/null
+#do
+#  printf "."
+#  sleep 3
+#done
 
-echo -e "\nmysql ready"
+#echo -e "\nmysql ready"
 
-echo -e "Starting Tomcat"
+#echo -e "Starting Tomcat"
 
 catalina.sh run
