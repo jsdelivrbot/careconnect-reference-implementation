ad407fdb60f79cfa921b9c9072e04bf2f9726b3b
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2018-09-06 15:49:48 +0100

    working on spring boot change over

--- ccri-fhir/src/main/java/uk/nhs/careconnect/ri/gateway/http/HAPIRestfulConfig.java
+++ ccri-fhir/src/main/java/uk/nhs/careconnect/ri/gateway/http/HAPIRestfulConfig.java
@@ -8,6 +8,8 @@ import ca.uhn.fhir.rest.server.FifoMemoryPagingProvider;
 import ca.uhn.fhir.rest.server.HardcodedServerAddressStrategy;
 import ca.uhn.fhir.rest.server.RestfulServer;
 import ca.uhn.fhir.util.VersionUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import uk.nhs.careconnect.ri.gatewaylib.provider.*;
@@ -22,9 +24,22 @@ import java.util.TimeZone;
 public class HAPIRestfulConfig extends RestfulServer {
 
 	private static final long serialVersionUID = 1L;
-	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(uk.nhs.careconnect.ri.gateway.http.HAPIRestfulConfig.class);
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(HAPIRestfulConfig.class);
 
-	private WebApplicationContext myAppCtx;
+	@Value("http://127.0.0.1/STU3")
+	private String serverBase;
+
+	@Value("${fhir.resource.serverName}")
+	private String serverName;
+
+	@Value("${fhir.resource.serverVersion}")
+	private String serverVersion;
+
+	private ApplicationContext applicationContext;
+
+	HAPIRestfulConfig(ApplicationContext context)  {
+		this.applicationContext = context;
+	}
 
     @Override
 	public void addHeadersToResponse(HttpServletResponse theHttpResponse) {
@@ -41,14 +56,15 @@ public class HAPIRestfulConfig extends RestfulServer {
 		FhirVersionEnum fhirVersion = FhirVersionEnum.DSTU3;
 		setFhirContext(new FhirContext(fhirVersion));
 
+		/*
 		// Get the spring context from the web container (it's declared in web.xml)
-		myAppCtx = ContextLoaderListener.getCurrentWebApplicationContext();
-		Config cfg = myAppCtx.getBean(uk.nhs.careconnect.ri.gateway.http.Config.class);
+		//applicationContext = ContextLoaderListener.getCurrentWebApplicationContext();
+		//Config cfg = applicationContext.getBean(uk.nhs.careconnect.ri.gateway.http.Config.class);
   
 		String serverBase = cfg.getServerBase();
 		String serverName = cfg.getServerName();
 		String serverVersion = cfg.getServerVersion();
-
+*/
 		log.info("serverBase: " + serverBase);
 		log.info("serverName: " + serverName);
 		log.info("serverVersion: " + serverVersion);
@@ -62,22 +78,22 @@ public class HAPIRestfulConfig extends RestfulServer {
 
 				// Care Connect API providers START
 
-				 myAppCtx.getBean(PatientResourceProvider.class)
-               	,myAppCtx.getBean(OrganisationResourceProvider.class)
-                ,myAppCtx.getBean(PractitionerResourceProvider.class)
-                ,myAppCtx.getBean(LocationResourceProvider.class)
-               	,myAppCtx.getBean(PractitionerRoleResourceProvider.class)
-      			,myAppCtx.getBean(ObservationResourceProvider.class) // Sprint 4 addition KGM
-				,myAppCtx.getBean(EncounterResourceProvider.class) // R3 addition KGM
-				,myAppCtx.getBean(ConditionResourceProvider.class) // R3 addition KGM
-				,myAppCtx.getBean(ProcedureResourceProvider.class) // R3 addition KGM
-				,myAppCtx.getBean(AllergyIntoleranceResourceProvider.class) // R3 addition KGM
-				,myAppCtx.getBean(MedicationRequestResourceProvider.class) // R3 addition KGM
-				,myAppCtx.getBean(MedicationStatementResourceProvider.class) // R3 addition KGM
-				,myAppCtx.getBean(ImmunizationResourceProvider.class) // R3 addition KGM
-				,myAppCtx.getBean(DocumentReferenceResourceProvider.class) // Unstructured
-				,myAppCtx.getBean(BinaryResourceProvider.class) // Unstructured
-				,myAppCtx.getBean(MedicationResourceProvider.class)
+				 applicationContext.getBean(PatientResourceProvider.class)
+               	,applicationContext.getBean(OrganisationResourceProvider.class)
+                ,applicationContext.getBean(PractitionerResourceProvider.class)
+                ,applicationContext.getBean(LocationResourceProvider.class)
+               	,applicationContext.getBean(PractitionerRoleResourceProvider.class)
+      			,applicationContext.getBean(ObservationResourceProvider.class) // Sprint 4 addition KGM
+				,applicationContext.getBean(EncounterResourceProvider.class) // R3 addition KGM
+				,applicationContext.getBean(ConditionResourceProvider.class) // R3 addition KGM
+				,applicationContext.getBean(ProcedureResourceProvider.class) // R3 addition KGM
+				,applicationContext.getBean(AllergyIntoleranceResourceProvider.class) // R3 addition KGM
+				,applicationContext.getBean(MedicationRequestResourceProvider.class) // R3 addition KGM
+				,applicationContext.getBean(MedicationStatementResourceProvider.class) // R3 addition KGM
+				,applicationContext.getBean(ImmunizationResourceProvider.class) // R3 addition KGM
+				,applicationContext.getBean(DocumentReferenceResourceProvider.class) // Unstructured
+				,applicationContext.getBean(BinaryResourceProvider.class) // Unstructured
+				,applicationContext.getBean(MedicationResourceProvider.class)
 
 				// Care Connect API providers END
 
@@ -85,21 +101,21 @@ public class HAPIRestfulConfig extends RestfulServer {
 
 				// NOT FOR LIVE - Use ENVIRONEMNT VARIABLE
 
-				,myAppCtx.getBean(QuestionnaireResourceProvider.class)
-				,myAppCtx.getBean(QuestionnaireResponseResourceProvider.class)
-				,myAppCtx.getBean(ListResourceProvider.class)
-				,myAppCtx.getBean(RelatedPersonResourceProvider.class)
-				,myAppCtx.getBean(CarePlanResourceProvider.class)
-				,myAppCtx.getBean(HealthcareServiceResourceProvider.class)
-				,myAppCtx.getBean(ReferralRequestResourceProvider.class)
-				,myAppCtx.getBean(CareTeamResourceProvider.class)
-				,myAppCtx.getBean(MedicationDispenseResourceProvider.class)
-				,myAppCtx.getBean(GoalResourceProvider.class)
-				,myAppCtx.getBean(RiskAssessmentResourceProvider.class)
-				,myAppCtx.getBean(ClinicalImpressionResourceProvider.class)
-				,myAppCtx.getBean(ConsentResourceProvider.class)
-				,myAppCtx.getBean(BundleResourceProvider.class) // Supports uploading resources
-				,myAppCtx.getBean(EpisodeOfCareResourceProvider.class) // TO DO Remove me for live KGM
+				,applicationContext.getBean(QuestionnaireResourceProvider.class)
+				,applicationContext.getBean(QuestionnaireResponseResourceProvider.class)
+				,applicationContext.getBean(ListResourceProvider.class)
+				,applicationContext.getBean(RelatedPersonResourceProvider.class)
+				,applicationContext.getBean(CarePlanResourceProvider.class)
+				,applicationContext.getBean(HealthcareServiceResourceProvider.class)
+				,applicationContext.getBean(ReferralRequestResourceProvider.class)
+				,applicationContext.getBean(CareTeamResourceProvider.class)
+				,applicationContext.getBean(MedicationDispenseResourceProvider.class)
+				,applicationContext.getBean(GoalResourceProvider.class)
+				,applicationContext.getBean(RiskAssessmentResourceProvider.class)
+				,applicationContext.getBean(ClinicalImpressionResourceProvider.class)
+				,applicationContext.getBean(ConsentResourceProvider.class)
+				,applicationContext.getBean(BundleResourceProvider.class) // Supports uploading resources
+				,applicationContext.getBean(EpisodeOfCareResourceProvider.class) // TO DO Remove me for live KGM
 
 				// Support for NHS Digital Natinal Projects  END
 
---
+++ ccri-fhir/src/main/java/uk/nhs/careconnect/ri/gateway/http/LoggingConfig.java
@@ -0,0 +1,19 @@
+package uk.nhs.careconnect.ri.gateway.http;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoggingConfig {
+    // See config details from http://hawt.io/plugins/logs/
+
+    /* Issue using logs, disable for now TODO
+    @Bean(name = "LogQuery", destroyMethod = "stop", initMethod = "start")
+    @Scope("singleton")
+    @Lazy(false)
+    public Log4jLogQuery log4jLogQuery() {
+        Log4jLogQuery log4jLogQuery = new Log4jLogQuery();
+        return log4jLogQuery;
+    }
+    */
+}
